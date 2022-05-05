7f69c17e61dca730e921f686b444fc538482e9c5
michael00987 <hyeongkwangseok@gmail.com>
2022-05-05 20:48:40 +0900

    타인이 수정함

--- L4/app.js
+++ L4/app.js
@@ -1,15 +1,15 @@
 // 이 파일은 node.js에서 실행 안됨 웹브라우저에서 가능
-
-console.log("hello")
+// 타인이 수정함
+console.log('hello');
 // console.log(document)
 // document=html
-console.log(document.body)
+console.log(document.body);
 
-let body = document.body
+let body = document.body;
 
-console.log(body===document.body)
+console.log(body === document.body);
 
-console.log(body.children[3])
+console.log(body.children[3]);
 // children
 
 // console.log(Array.isArray(body.children)) 배열인지 확일할때 결과 타입은 boolean으로
@@ -23,6 +23,5 @@ console.log(body.children[3])
 // let lonely = document.getElementsByClassName("lonely")
 // console.log(lonely)
 
-let lonely = document.querySelectorAll("#lonely")
-console.log(lonely)
-
+let lonely = document.querySelectorAll('#lonely');
+console.log(lonely);
