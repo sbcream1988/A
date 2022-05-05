d471e7380016b56301d240250d3eda0e4d4283f9
sbcream1988 <sbcream1988@gmail.com>
2022-05-05 20:24:37 +0900

    first homework

---
+++ HomeWork/app.js
@@ -0,0 +1,40 @@
+let output=isOldEnoughToDrink(22);
+console.log(output)
+
+
+function isOldEnoughToDrink(age) {
+    return age >= 21
+    if(age >= 21){
+        return true;
+    }else {
+        return false;
+    }
+}
+
+
+
+// let output = checkAge('Adrian',22);
+// console.log(output)
+
+// let output2 = checkAge('John',17);
+// console.log(output2)
+
+// function checkAge(name,age){
+//     if (age<21) {
+//         return "Go Home, " + name + "!";
+//     } else {
+//         return "Welcome, " + name + "!";
+//     }
+// }
+
+// let output = equalsTen(9)
+// console.log(" =10: ", output);
+
+// function equalsTen(num){
+//     return num === 10
+//     if(num === 10) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
