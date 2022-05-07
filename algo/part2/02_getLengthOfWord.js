ae159421835364105b26ada0761088d083ef4dee
michael00987 <hyeongkwangseok@gmail.com>
2022-05-07 16:16:24 +0900

    문자열

---
+++ algo/part2/02_getLengthOfWord.js
@@ -0,0 +1,13 @@
+/*
+
+Given a word, "getLengthOfWord" returns the length of the given word 
+(단어가 주어졌을때, "getLengthOfWord"함수는 단어의 기이를 반환해야 합니다.)
+*/
+
+let output = getLengthOfWord('some');
+console.log(output); // -> 4
+
+function getLengthOfWord(str) {
+  // TODO
+  return;
+}
---
+++ algo/part2/07_areValidCredentials.js
@@ -0,0 +1,13 @@
+/*
+Given a name and a password "areValidCredentials", returns true if the name is longer than 3 characters,
+AND, the password is at least 8 characters long, Otherwise it returns false
+이름과 비밀번호가 주어졌을때, "areValidCredentials" 함수는 이름이 3글자보다 길고 비밀번호가 8글자 이상이면 true를 반환합니다.
+*/
+
+let output = areValidCredentials('Ritu', 'mylongpassword');
+console.log(ouput); // --> true
+
+function areValidCredentials(name, password) {
+  // TODO
+  return;
+}
